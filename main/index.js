9281e9a15c897a08964497029d033cb65f1dbccd
Vitaly Kuznetsov <vitalyq@gmail.com>
2017-10-29 21:51:34 +0300

    Implement basic client

--- main/index.js
+++ main/index.js
@@ -20,3 +20,33 @@
 //
 // Nyan cat lies here...
 //
+const URL = 'ws://localhost:3000/';
+
+App.onToken = (token) => {
+  const socket = new WebSocket(`${URL}?token=${token}`);
+  let lifeGame;
+
+  const handleMessage = (event) => {
+    const { type, data } = JSON.parse(event.data);
+
+    if (type === 'INITIALIZE') {
+      lifeGame = new LifeGame(data.user, data.settings);
+      lifeGame.init();
+      lifeGame.setState(data.state);
+      lifeGame.send = (data) => {
+        socket.send(JSON.stringify({
+          type: 'ADD_POINT',
+          data,
+        }));
+      };
+    } else if (type === 'UPDATE_STATE') {
+      lifeGame.setState(data);
+    }
+  };
+
+  // Set up listeners
+  socket.addEventListener('message', handleMessage);
+  socket.addEventListener('open', () => console.log('Connected'));
+  socket.addEventListener('close', () => console.log('Disconnected'));
+  socket.addEventListener('error', console.error);
+};
