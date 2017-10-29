e344b979b98ddd03e5827993a1bebde8ff455ed0
Vitaly Kuznetsov <vitalyq@gmail.com>
2017-10-29 23:24:50 +0300

    Batch point addition

--- main/index.js
+++ main/index.js
@@ -21,25 +21,46 @@
 // Nyan cat lies here...
 //
 const URL = 'ws://localhost:3000/';
+const ADD_INTERVAL = 1000;
 
 App.onToken = (token) => {
+  if (token.length === 0) { return; }
   const socket = new WebSocket(`${URL}?token=${token}`);
+  const points = [];
   let lifeGame;
+  let lastTimeout;
+  let color;
+
+  const handleSend = (data) => {
+    // Batch some points and send in bulk
+    points.push(...data.affectedPoints);
+
+    const timeout = window.setTimeout(() => {
+      if (timeout !== lastTimeout) { return; }
+      data.affectedPoints = points.concat();
+      points.length = 0;
+      socket.send(JSON.stringify({
+        type: 'ADD_POINT',
+        data,
+      }));
+    }, ADD_INTERVAL);
+    lastTimeout = timeout;
+  };
 
   const handleMessage = (event) => {
     const { type, data } = JSON.parse(event.data);
 
     if (type === 'INITIALIZE') {
+      color = data.user.color;
       lifeGame = new LifeGame(data.user, data.settings);
       lifeGame.init();
       lifeGame.setState(data.state);
-      lifeGame.send = (data) => {
-        socket.send(JSON.stringify({
-          type: 'ADD_POINT',
-          data,
-        }));
-      };
+      lifeGame.send = handleSend;
     } else if (type === 'UPDATE_STATE') {
+      // Apply batched points
+      points.forEach((p) => {
+        data[p.x][p.y] = { user: { color, token }};
+      });
       lifeGame.setState(data);
     }
   };
